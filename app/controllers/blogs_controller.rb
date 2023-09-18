67d64a0bc74119ab2b0e618d2b44bd0c40b7ff78
Junichi Ito <jit@sonicgarden.jp>
2023-09-18 21:00:57 +0900

    Add test for 422 error (but fails)

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -22,16 +22,8 @@ class BlogsController < ApplicationController
   # POST /blogs or /blogs.json
   def create
     @blog = Blog.new(blog_params)
-
-    respond_to do |format|
-      if @blog.save
-        format.html { redirect_to blog_url(@blog), notice: "Blog was successfully created." }
-        format.json { render :show, status: :created, location: @blog }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @blog.errors, status: :unprocessable_entity }
-      end
-    end
+    @blog.save!
+    redirect_to blog_url(@blog), notice: "Blog was successfully created."
   end
 
   # PATCH/PUT /blogs/1 or /blogs/1.json
--- test/system/blogs_test.rb
+++ test/system/blogs_test.rb
@@ -1,43 +1,25 @@
 require "application_system_test_case"
 
 class BlogsTest < ApplicationSystemTestCase
-  setup do
-    @blog = blogs(:one)
-  end
-
-  test "visiting the index" do
-    visit blogs_url
-    assert_selector "h1", text: "Blogs"
-  end
-
-  test "should create blog" do
+  test "creating a valid Blog" do
     visit blogs_url
     click_on "New blog"
 
-    fill_in "Content", with: @blog.content
-    fill_in "Title", with: @blog.title
+    fill_in "Title", with: 'Hello'
+    fill_in "Content", with: 'World!'
     click_on "Create Blog"
 
     assert_text "Blog was successfully created"
-    click_on "Back"
   end
 
-  test "should update Blog" do
-    visit blog_url(@blog)
-    click_on "Edit this blog", match: :first
+  test "creating an invalid Blog" do
+    visit blogs_url
+    click_on "New blog"
 
-    fill_in "Content", with: @blog.content
-    fill_in "Title", with: @blog.title
-    click_on "Update Blog"
+    fill_in "Title", with: ''
+    fill_in "Content", with: 'World!'
+    click_on "Create Blog"
 
-    assert_text "Blog was successfully updated"
-    click_on "Back"
-  end
-
-  test "should destroy Blog" do
-    visit blog_url(@blog)
-    click_on "Destroy this blog", match: :first
-
-    assert_text "Blog was successfully destroyed"
+    assert_text "The change you wanted was rejected." # fails
   end
 end
